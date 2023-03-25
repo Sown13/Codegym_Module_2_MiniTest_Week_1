c563008bf3c5bd5dafd1d1ed2a24d1901ccd941b
Sown13 <sonnh.buh@gmail.com>
2023-03-25 18:32:02 +0700

    Ex 2,3 - done

--- src/ArrayInit.java
+++ src/ArrayInit.java
@@ -1,5 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayInit {
     public static void main(String[] args) {
+        int[] arr = initArray();
+        System.out.println(Arrays.toString(arr));
+        System.out.println("Sum of this array is: " + sumArray(arr));
+        System.out.println("The max of this array is: " + findMax(arr));
     }
-    public static void
+    public static int[] initArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your length ");
+        int length = scanner.nextInt();
+        int[] customArr = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.printf("Enter your array[%d] ", i);
+            customArr[i] = scanner.nextInt();
+        }
+        return customArr;
+    }
+    public static int sumArray(int[] arr) {
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum;
+    }
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int num : arr){
+            if (max < num){
+                max = num;
+            }
+        } return max;
+    }
 }
