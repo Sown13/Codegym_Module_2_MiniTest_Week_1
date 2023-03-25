8e45d606888bc7aabd1f957b424bed85ec8b2514
Sown13 <sonnh.buh@gmail.com>
2023-03-25 19:23:58 +0700

    Change name main class

--- src/StudentInfor.java
+++ src/StudentInfor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class StudentInfor {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the student name");
