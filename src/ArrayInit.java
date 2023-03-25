a2a13382cb8728d62b0f7b76a4ebbfbb94c7cba0
Sown13 <sonnh.buh@gmail.com>
2023-03-25 17:47:59 +0700

    Fix ex1

---
+++ src/ArrayInit.java
@@ -0,0 +1,5 @@
+public class ArrayInit {
+    public static void main(String[] args) {
+    }
+    public static void
+}
--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -23,7 +23,7 @@ public class PrimeNumber {
     public static boolean isPrime(int num) {
         if (num < 2) return false;
         else {
-            for (int i = 2; i < Math.sqrt(num); i++) {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
                 if (num % i == 0) return false;
             }
         }
