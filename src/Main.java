290f766d65c4578b49dce60613242668b169afcf
Sown13 <sonnh.buh@gmail.com>
2023-03-25 19:14:02 +0700

    Ex 4 - done

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the student name");
+        String name = scanner.nextLine();
+        System.out.println("Enter the student class");
+        String sClass = scanner.nextLine();
+        System.out.println("Enter the student age");
+        byte age = scanner.nextByte();
+        Student studentWhoKnow = new Student(name, sClass, age);
+        System.out.print("The student: " + studentWhoKnow.getStudentName());
+        System.out.print(" of class " + studentWhoKnow.getStudentClass());
+        System.out.print(" is " + studentWhoKnow.getStudentAge() + " years old");
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    String studentName, studentClass;
+    byte studentAge;
+    public Student(String name, String sClass, byte age){
+        this.studentName = name;
+        this.studentClass = sClass;
+        this.studentAge = age;
+    }
+    public void setName(String name){
+        this.studentName = name;
+    }
+    public void setClass(String sClass){
+        this.studentClass = sClass;
+    }
+    public void setAge(byte age){
+        this.studentAge = age;
+    }
+    public String getStudentName (){
+        return this.studentName;
+    }
+    public String getStudentClass(){
+        return this.studentClass;
+    }
+    public byte getStudentAge(){
+        return this.studentAge;
+    }
+}
